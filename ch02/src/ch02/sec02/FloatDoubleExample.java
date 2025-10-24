b13beabfea9b2962a488a0642f8bff91b6b5cb28
E. K. Lee <wolf@teoal.net>
2025-10-24 21:34:56 +0900

    02장 05절까지 실습

---
+++ ch02/src/ch02/sec02/FloatDoubleExample.java
@@ -0,0 +1,21 @@
+package ch02.sec02;
+
+public class FloatDoubleExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float var1 = 0.12345678901234567489f;	// 소수 형식 리터럴은 기본이 double, float에 대입하면 오류
+		double var2 = 0.1234567890123456789;
+		System.out.println(var1);
+		System.out.println(var2);
+		
+		double var3 = 3e6;
+		float var4 = 3e6f;	// 마찬가지로 이것도 double로 보니 float에 대입하면 오류
+		double var5 = 2e-3;
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+		
+	}
+
+}
---
+++ ch02/src/ch02/sec02/LongExample.java
@@ -0,0 +1,13 @@
+package ch02.sec02;
+
+public class LongExample {
+
+	public static void main(String[] args) {
+		long var1 = 10;
+		long var2 = 20L;
+		long var3 = 10000000000;	// 리터럴 숫자는 기본적으로 int, 21억을 넘으면 오류
+		long var4 = 10000000000L;
+
+	}
+
+}
